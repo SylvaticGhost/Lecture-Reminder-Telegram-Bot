521baa2ec435f9c544d41794b699f77454b8992a
SylvaticGhost <143171907+SylvaticGhost@users.noreply.github.com>
2024-02-22 14:29:29 +0200

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -4,9 +4,9 @@ namespace LectureReminder;
 
 public class Config
 {
-    public string BotToken { get; } = "6876162322:AAGYMpQ8_mazCnwJHgIMnIpAoPbHVWeAGew";
-    public string ChatId { get;  } = "-1002142606811";
-    public string ConnectionString { get; } = "DefaultEndpointsProtocol=https;AccountName=clonesptstorage;AccountKey=9gF3xUCthAPZU/85EarH5y+d9bxtAPkAh0/Mi8VFq2JGyuAKsBPwZuzaFY+IUvhukZ+2R4HFbKfl+ASt8teHDw==;EndpointSuffix=core.windows.net";
+    public string BotToken { get; } = "";
+    public string ChatId { get;  } = "";
+    public string ConnectionString { get; } = "";
     public string TableName { get; } = "LectureSchedule";
     public DateTime StartingDate { get; } = new (2024, 1, 22);
 }
